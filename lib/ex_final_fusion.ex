58c22ec7150fc1f2f228a85ca0e6521f9eb8b469
Daniel Kukula <daniel.kuku@gmail.com>
2024-04-13 10:36:02 +0100

    add types

--- lib/ex_final_fusion.ex
+++ lib/ex_final_fusion.ex
@@ -22,12 +22,62 @@ defmodule ExFinalFusion.Native do
     otp_app: :ex_final_fusion,
     crate: "exfinalfusion_native"
 
+  @type read_type ::
+          :floret_text
+          | :embeddings
+          | :mmap_embeddings
+          | :fasttext
+          | :fasttext_lossy
+          | :text
+          | :text_lossy
+          | :text_dims
+          | :text_dims_lossy
+          | :word2vec_binary
+          | :word2vec_binary_lossy
+          | :fifu
+          | :word2vec
+          | :floret
+  @doc """
+  Functions available on embeddings module
+
+  - :floret_text,
+  - :embeddings,
+  - :mmap_embeddings,
+  - :fasttext,
+  - :fasttext_lossy,
+  - :text,
+  - :text_lossy,
+  - :text_dims,
+  - :text_dims_lossy,
+  - :word2vec_binary,
+  - :word2vec_binary_lossy,
+
+  Aliases
+
+  - :fifu = :embeddings,
+  - :word2vec = :word2vec_binary,
+  - :floret = :floret_text
+  """
+  @spec read(String.t(), read_type) :: reference()
   def read(_arg1, _arg2), do: err()
+
+  @doc """
+  returns embedding of a word
+  """
+  @spec embedding(reference(), String.t()) :: {:ok, [float]}
   def embedding(_arg1, _arg2), do: err()
+
+  @doc """
+  returns a vector of embeddings of a word
+  """
+  @spec embedding(reference(), [String.t()]) :: {:ok, [[float]]}
   def embedding_batch(_arg1, _arg2), do: err()
 
+  @spec len(reference()) :: integer
   def len(_arg1), do: err()
+  @spec dims(reference()) :: [integer]
   def dims(_arg1), do: err()
+  @spec metadata(reference()) :: map() | nil
   def metadata(_arg1), do: err()
 
   defp err, do: :erlang.nif_error(:nif_not_loaded)
